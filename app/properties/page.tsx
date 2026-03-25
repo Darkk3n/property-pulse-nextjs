e0693d044869c8389255a6bb5cbeeb7bf1a003e3
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-03-24 22:19:48 -0600

    Added base property listing

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,5 +1,17 @@
+import properties from '@/properties.json';
+
 const PropertiesPage = () => {
-    return (<section>Properties Page</section>);
+    return (<section className='px-4 py-6'>
+        <div className='container-xl lg:container m-auto px-4 py-6'>
+            {properties.length === 0 ? (<p>No Properties found</p>) : (
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                    {properties.map((p) => (
+                        <div>{p.name}</div>
+                    ))}
+                </div>
+            )}
+        </div>
+    </section>);
 }
 
 export default PropertiesPage;
