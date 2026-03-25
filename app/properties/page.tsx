0ef207fab9e65e5fb07b74c2eca9486ad40b92b9
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-03-24 22:48:56 -0600

    Added PropertyCard component

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,12 +1,16 @@
+import PropertyCard from '@/components/PropertyCard';
+import { Property } from '@/models/types';
 import properties from '@/properties.json';
 
 const PropertiesPage = () => {
+    const propertiesData: Property[] = properties as Property[];
+
     return (<section className='px-4 py-6'>
         <div className='container-xl lg:container m-auto px-4 py-6'>
-            {properties.length === 0 ? (<p>No Properties found</p>) : (
+            {propertiesData.length === 0 ? (<p>No Properties found</p>) : (
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                    {properties.map((p) => (
-                        <div>{p.name}</div>
+                    {propertiesData.map((p) => (
+                        <PropertyCard key={p._id} property={p} />
                     ))}
                 </div>
             )}
