da0890eba5054d5e08cb5406f3ab35d6bb88f82e
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-03-25 14:13:20 -0600

    Added interface

--- models/Property.ts
+++ models/Property.ts
@@ -1,4 +1,4 @@
-import { Schema, model, models } from 'mongoose';
+import { InferSchemaType, Schema, model, models } from 'mongoose';
 
 const PropertySchema: Schema = new Schema({
     owner: {
@@ -52,5 +52,37 @@ const PropertySchema: Schema = new Schema({
         default: false
     }
 }, { timestamps: true })
+
+export interface IProperty {
+    _id: string;
+    owner: string;
+    name: string;
+    type: string;
+    description?: string; // Optional since it's not required in schema
+    location: {
+        street?: string;
+        city?: string;
+        state?: string;
+        zipcode?: string;
+    };
+    beds: number;
+    baths: number;
+    square_feet: number;
+    amenities: string[];
+    rates: {
+        nightly?: number;
+        weekly?: number;
+        monthly?: number;
+    };
+    seller_info: {
+        name?: string;
+        email?: string;
+        phone?: string;
+    };
+    images: string[];
+    is_featured: boolean;
+    createdAt: string; // From timestamps: true
+    updatedAt: string; // From timestamps: true
+}
 
 export const Property = models.Property || model('Property', PropertySchema)
