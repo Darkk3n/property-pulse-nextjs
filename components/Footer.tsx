997d34e0963025c438a48bc7d1b979cc38a4521d
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-03-24 20:01:16 -0600

    Changed images and link in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,28 @@
+import logo from '@/assets/images/logo.png';
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-    return <footer className="bg-gray-200 py-4 mt-auto">
+    const currentYear = new Date().getFullYear();
+
+    return <footer className="bg-gray-200 py-4 mt-24">
         <div
             className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4"
         >
             <div className="mb-4 md:mb-0">
-                <img src="/images/logo.png" alt="Logo" className="h-8 w-auto" />
+                <Image src={logo} alt="Logo" className="h-8 w-auto" />
             </div>
             <div
                 className="flex flex-wrap justify-center md:justify-start mb-4 md:mb-0"
             >
                 <ul className="flex space-x-4">
-                    <li><a href="/properties.html">Properties</a></li>
-                    <li><a href="/terms.html">Terms of Service</a></li>
+                    <li><Link href="/properties">Properties</Link></li>
+                    <li><Link href="/">Terms of Service</Link></li>
                 </ul>
             </div>
             <div>
                 <p className="text-sm text-gray-500 mt-2 md:mt-0">
-                    &copy; 2026 PropertyPulse. All rights reserved.
+                    &copy; {currentYear} Property Pulse. All rights reserved.
                 </p>
             </div>
         </div>
