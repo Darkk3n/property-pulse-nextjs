6795221867338d62082aaf768ef29c46ab6486ab
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-03-25 14:20:11 -0600

    Property details

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,3 +1,4 @@
+import PropertyDetails from '@/components/PropertyDetails';
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import connectDb from '@/config/database';
 import { Property } from '@/models/Property';
@@ -17,10 +18,7 @@ const PropertyPage = async ({ params }: PropertyPageProps) => {
             <PropertyHeaderImage image={property.images[0]} />
             <section>
                 <div className="container m-auto py-6 px-6">
-                    <Link
-                        href="/properties"
-                        className="text-blue-500 hover:text-blue-600 flex items-center"
-                    >
+                    <Link href="/properties" className="text-blue-500 hover:text-blue-600 flex items-center">
                         <FaArrowLeft className='mr-2' /> Back to Properties
                     </Link>
                 </div>
@@ -28,7 +26,7 @@ const PropertyPage = async ({ params }: PropertyPageProps) => {
             <section className="bg-blue-50">
                 <div className="container m-auto py-10 px-6">
                     <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-
+                        <PropertyDetails property={property} />
                     </div>
                 </div>
             </section>
