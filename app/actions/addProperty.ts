cad66707d78837fd68f2349d457844a96413259e
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-03-26 16:15:09 -0600

    Added server action

---
+++ app/actions/addProperty.ts
@@ -0,0 +1,58 @@
+'use server'
+import connectDb from "@/config/database";
+import { Property } from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+async function addProperty(formData: FormData) {
+   await connectDb();
+
+   const sessionUser = await getSessionUser();
+   if (!sessionUser || !sessionUser.userId) {
+      throw new Error('User ID is required');
+   }
+   const { userId } = sessionUser;
+   // Access all values from amenities and images
+   const amenities = formData.getAll('amenities')
+   const images = formData
+      .getAll('images')
+      .filter((i) => i instanceof File && i.name !== '')
+      .map((i) => (i as File).name);
+
+   const propertyData = {
+      owner: userId,
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+         street: formData.get('location.street'),
+         city: formData.get('location.city'),
+         zipcode: formData.get('location.zipcode'),
+
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      square_feet: formData.get('square_feet'),
+      amenities,
+      rates: {
+         nightly: formData.get('rates.nightly'),
+         weekly: formData.get('rates.weekly'),
+         monthly: formData.get('rates.monthly'),
+      },
+      seller_info: {
+         name: formData.get('seller_info.name'),
+         email: formData.get('seller_info.email'),
+         phone: formData.get('seller_info.phone'),
+      },
+      images,
+   }
+   console.log(propertyData)
+   const newProperty = new Property(propertyData);
+   await newProperty.save()
+
+   revalidatePath('/', 'layout')
+   redirect(`/properties/${newProperty._id}`);
+}
+
+export default addProperty
