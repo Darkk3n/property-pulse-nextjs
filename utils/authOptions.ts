9c785ba4ba09ff7cf0026255a8dce31eac678bb5
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-03-26 13:46:48 -0600

    Fixed profile image

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -17,9 +17,12 @@ export const authOptions: NextAuthOptions = {
    })],
    callbacks: {
       // Invoked on successful sign in 
-      async signIn({ profile }) {
+      async signIn({ profile, account }) {
          // 1. Connect to DB
          await connectDb();
+         const profileImage = account?.provider === "google"
+            ? (profile as any)?.picture
+            : profile?.image;
          // 2. Check if user exists
          const userExists = await User.findOne({ email: profile?.email })
          // 3. if not, create user
@@ -29,7 +32,7 @@ export const authOptions: NextAuthOptions = {
             await User.create({
                email: profile?.email,
                username: userName,
-               image: profile?.image
+               image: profileImage
             })
          }
          // 4. Return true to allow sign in
