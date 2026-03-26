d86578462390cbf3af369ed105eb136dbd7b25e1
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-03-26 13:33:33 -0600

    Implemented callbacks login

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,3 +1,5 @@
+import connectDb from "@/config/database";
+import { User } from "@/models/User";
 import { NextAuthOptions } from "next-auth";
 import Google from "next-auth/providers/google";
 
@@ -17,15 +19,30 @@ export const authOptions: NextAuthOptions = {
       // Invoked on successful sign in 
       async signIn({ profile }) {
          // 1. Connect to DB
+         await connectDb();
          // 2. Check if user exists
+         const userExists = await User.findOne({ email: profile?.email })
          // 3. if not, create user
+         if (!userExists) {
+            // Truncate userName if too long
+            const userName = profile?.name?.slice(0, 20);
+            await User.create({
+               email: profile?.email,
+               username: userName,
+               image: profile?.image
+            })
+         }
          // 4. Return true to allow sign in
          return true;
       },
       // Session callback function that modifies the session object
       async session({ session }) {
          // 1. Get user from DB
+         const user = await User.findOne({ email: session.user?.email })
          // 2. Assign user id from sessions
+         if (session.user) {
+            session.user.id = user._id.toString()
+         }
          // 3. Return session
          return session;
       }
