3002333847b0bb3d87f1fcfb656df637ea58d396
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-03-27 17:16:57 -0600

    Closed profile dropdown on navigate

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -188,6 +188,7 @@ const NavBar = () => {
                                             role="menuitem"
                                             // tabIndex="-1"
                                             id="user-menu-item-0"
+                                            onClick={() => setIsProfileMenuOpen(false)}
                                         >Your Profile</Link>
                                         <Link
                                             href="/properties/saved"
@@ -217,33 +218,35 @@ const NavBar = () => {
         </div>
 
         {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-        {isMobileMenuOpen && (
-            <div id="mobile-menu">
-                <div className="space-y-1 px-2 pb-3 pt-2">
-                    <Link
-                        href="/"
-                        className={`${pathName === '/' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
-                    >Home</Link>
-                    <Link
-                        href="/properties"
-                        className={`${pathName === '/properties' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
-                    >Properties</Link>
-                    {session && (
+        {
+            isMobileMenuOpen && (
+                <div id="mobile-menu">
+                    <div className="space-y-1 px-2 pb-3 pt-2">
                         <Link
-                            href="/properties/add"
-                            className={`${pathName === '/properties/add' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
-                        >Add Property</Link>
-                    )}
-                    <button
-                        className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
-                    >
-                        <i className="fa-brands fa-google mr-2"></i>
-                        <span>Login or Register</span>
-                    </button>
+                            href="/"
+                            className={`${pathName === '/' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
+                        >Home</Link>
+                        <Link
+                            href="/properties"
+                            className={`${pathName === '/properties' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
+                        >Properties</Link>
+                        {session && (
+                            <Link
+                                href="/properties/add"
+                                className={`${pathName === '/properties/add' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
+                            >Add Property</Link>
+                        )}
+                        <button
+                            className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
+                        >
+                            <i className="fa-brands fa-google mr-2"></i>
+                            <span>Login or Register</span>
+                        </button>
+                    </div>
                 </div>
-            </div>
-        )}
-    </nav>);
+            )
+        }
+    </nav >);
 }
 
 export default NavBar;
