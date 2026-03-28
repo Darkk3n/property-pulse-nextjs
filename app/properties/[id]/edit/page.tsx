f0035cd9639222b3e086a3067ae60a4690d48f36
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-03-27 22:45:03 -0600

    Add values to form

---
+++ app/properties/[id]/edit/page.tsx
@@ -0,0 +1,21 @@
+import PropertyEditForm from "@/components/PropertyEditForm";
+import connectDb from "@/config/database";
+import { IProperty, Property } from "@/models/Property";
+import { convertToSerializableObject } from "@/utils/convertToObject";
+
+const PropertyEditPage = async ({ params }: { params: { id: string } }) => {
+   await connectDb();
+   const propertyDoc = await Property.findById(params.id).lean();
+   const property: IProperty = convertToSerializableObject(propertyDoc);
+   if (!property)
+      return <h1 className="text-center text-2xl font-bold mt-10">Property Not Found</h1>
+   return (<section className="bg-blue-50">
+      <div className="container m-auto max-w-2xl py-24">
+         <div className="bg-white px-6 py-6 mb-4 shadow-md rounded-md border m-4 md:m-0">
+            <PropertyEditForm property={property} />
+         </div>
+      </div>
+   </section>);
+}
+
+export default PropertyEditPage;
