5f82362ed67e3459689278ff47b8edf287e96632
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-03-31 16:57:34 -0600

    Implemented Lightbox

--- components/PropertyImages.tsx
+++ components/PropertyImages.tsx
@@ -1,33 +1,60 @@
+'use client'
 import Image from "next/image";
+import { Gallery, Item } from "react-photoswipe-gallery";
 
 const PropertyImages = ({ images }: { images: string[] }) => {
-   return (<section className="bg-blue-50 p-4">
-      <div className="container mx-auto">
-         {images.length === 1 ? (
-            <Image src={images[0]}
-               alt=""
-               className="object-cover h-[400p] mx-auto rounded-xl"
-               width={1800}
-               height={400}
-               priority
-            />
-         ) : (
-            <div className="grid grid-cols-2 gap-4">
-               {images.map((i, idx) => (
-                  <div key={idx} className={`${images.length === 3 && idx === 2 ? 'col-span-2' : 'col-span-1'}`}>
-                     <Image src={i}
-                        alt=""
-                        className="object-cover h-[400p] w-full rounded-xl"
-                        width={1800}
-                        height={400}
-                        priority
-                     />
+   return (
+      <Gallery>
+         <section className="bg-blue-50 p-4">
+            <div className="container mx-auto">
+               {images.length === 1 ? (
+                  <Item
+                     original={images[0]}
+                     thumbnail={images[0]}
+                     width='1000'
+                     height='600'
+                  >
+                     {({ ref, open }) => (
+                        <Image src={images[0]}
+                           alt=""
+                           ref={ref}
+                           onClick={open}
+                           className="object-cover h-[400p] mx-auto rounded-xl cursor-pointer"
+                           width={1800}
+                           height={400}
+                           priority
+                        />
+                     )}
+                  </Item>
+               ) : (
+                  <div className="grid grid-cols-2 gap-4">
+                     {images.map((i, idx) => (
+                        <div key={idx} className={`${images.length === 3 && idx === 2 ? 'col-span-2' : 'col-span-1'}`}>
+                           <Item
+                              original={i}
+                              thumbnail={i}
+                              width='1000'
+                              height='600'
+                           >
+                              {({ ref, open }) => (
+                                 <Image src={i}
+                                    alt="" ref={ref}
+                                    onClick={open}
+                                    className="object-cover h-[400p] w-full rounded-xl cursor-pointer"
+                                    width={1800}
+                                    height={400}
+                                    priority
+                                 />
+                              )}
+                           </Item>
+                        </div>
+                     ))}
                   </div>
-               ))}
+               )}
             </div>
-         )}
-      </div>
-   </section>);
+         </section>
+      </Gallery>
+   );
 }
 
 export default PropertyImages;
