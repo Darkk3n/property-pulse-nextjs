4a31b62ed470745a76e39e70ea6f2a79fef2bd1f
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-03-31 10:00:56 -0600

    Created messages page. Fixed notification click to close menu

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -123,6 +123,7 @@ const NavBar = () => {
                             <button
                                 type="button"
                                 className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                onClick={() => setIsProfileMenuOpen(false)}
                             >
                                 <span className="absolute -inset-1.5"></span>
                                 <span className="sr-only">View notifications</span>
